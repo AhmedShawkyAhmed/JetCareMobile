6d4eb0377da7e01c439a3f290bde4b177a5bcc00
ahmed_shawky <shawkyahmed392@gmail.com>
2023-05-28 14:43:43 +0300

    notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,8 @@ import UIKit
 import Flutter
 import GoogleMaps
 import FirebaseCore
+import FirebaseMessaging
+import flutter_local_notifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -12,6 +14,26 @@ import FirebaseCore
     GMSServices.provideAPIKey("AIzaSyA4LqOxrIrI4oZXDAU1vWMUGoE5xuWTY5s")
       FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+      }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      // Pass device token to messaging
+      Messaging.messaging().apnsToken = deviceToken
+
+      return super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+  override func application(_ application: UIApplication,
+                            didReceiveRemoteNotification notification: [AnyHashable : Any],
+                            fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+      // Handle it for firebase messaging analytics
+      if ((notification["gcm.message_id"]) != nil) {
+          Messaging.messaging().appDidReceiveMessage(notification)
+      }
+
+      return super.application(application, didReceiveRemoteNotification: notification, fetchCompletionHandler: completionHandler)
   }
 }
