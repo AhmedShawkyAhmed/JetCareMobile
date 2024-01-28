999a21c6f3ad87642dd4bcc9fccc9107a21e85e1
ahmed_shawky_24 <ahmedshawkyahmed392@gmail.com>
2024-01-28 11:03:04 +0200

    update flutter

--- lib/src/constants/constants_methods.dart
+++ lib/src/constants/constants_methods.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/foundation.dart';
 
-void printResponse(String text) {
+void printResponse(Object? object) {
+  String text = "$object";
   if (kDebugMode) {
     print('\x1B[33m$text\x1B[0m');
   }
 }
 
-void printSuccess(String text) {
+void printSuccess(Object? object) {
+String text = "$object";
   if (kDebugMode) {
     print('\x1B[32m$text\x1B[0m');
   }
 }
 
-void printError(String text) {
+void printError(Object? object) {
+  String text = "$object";
   if (kDebugMode) {
     print('\x1B[31m$text\x1B[0m');
   }
 }
 
-void printInfo(String text) {
+void printInfo(Object? object) {
+  String text = "$object";
   if (kDebugMode) {
     print('\x1B[34m$text\x1B[0m');
   }
